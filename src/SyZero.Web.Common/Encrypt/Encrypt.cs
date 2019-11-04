30299dd260c36573d6d92c73de2c3022ccd63a72
syzero <522112669@qq.com>
2019-11-04 23:11:44 +0800

    .

---
+++ src/SyZero.Web.Common/Encrypt/Encrypt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyZero.Web.Common
+{
+    public class Encrypt
+    {
+
+
+
+
+    }
+}
---
+++ src/SyZero.Web.Common/Xml/XmlSerialize.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace SyZero.Web.Common
+{
+    public class XmlSerialize : IXmlSerialize
+    {
+        public bool AppendChild(string filePath, string xPath, XmlNode xmlNode)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filePath);
+                XmlNode xn = doc.SelectSingleNode(xPath);
+                XmlNode n = doc.ImportNode(xmlNode, true);
+                xn.AppendChild(n);
+                doc.Save(filePath);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool AppendChild(string filePath, string xPath, string toFilePath, string toXPath)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(toFilePath);
+                XmlNode xn = doc.SelectSingleNode(toXPath);
+
+                XmlNodeList xnList = ReadNodes(filePath, xPath);
+                if (xnList != null)
+                {
+                    foreach (XmlElement xe in xnList)
+                    {
+                        XmlNode n = doc.ImportNode(xe, true);
+                        xn.AppendChild(n);
+                    }
+                    doc.Save(toFilePath);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public XmlDocument LoadXmlDoc(string filePath)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filePath);
+                return doc;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public XmlNodeList ReadNodes(string filePath, string xPath)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filePath);
+                XmlNode xn = doc.SelectSingleNode(xPath);
+                XmlNodeList xnList = xn.ChildNodes;  //得到该节点的子节点
+                return xnList;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool UpdateNodeInnerText(string filePath, string xPath, string value)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filePath);
+                XmlNode xn = doc.SelectSingleNode(xPath);
+                XmlElement xe = (XmlElement)xn;
+                xe.InnerText = value;
+                doc.Save(filePath);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
