49d8835bf975cd718a91a6bf942316f2f8b623c3
winter2048 <522112669@qq.com>
2023-03-14 00:07:49 +0800

    update

--- .github/workflows/script/push_package.cmd
+++ .github/workflows/script/push_package.cmd
@@ -8,6 +8,6 @@ cd %~dp0..\..\..\nuget
 for /f "delims=" %%a in ('dir /b/a-d/oN *.nupkg') do (
     echo ""
     echo %%a: 开始推送
-    dotnet nuget push "%%a" --api-key "%GITHUB_NUGET_TOKEN%" --source "%source_url%"
+    dotnet nuget push "%%a" --api-key "%GB_NUGET_TOKEN%" --source "%source_url%" --skip-duplicate
     echo %%a: 推送完成
 )
