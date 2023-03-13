7dae93600e27185859f9b16610456566f4eff83a
winter2048 <522112669@qq.com>
2023-03-14 00:30:54 +0800

    update

--- .github/workflows/script/push_package.cmd
+++ .github/workflows/script/push_package.cmd
@@ -1,13 +1,10 @@
 @echo off
 
-set source_url="https://nuget.pkg.github.com/winter2048/index.json"
+echo 推送至%2
 
-echo 推送至%source_url%
-
-cd %~dp0..\..\..\nuget
-for /f "delims=" %%a in ('dir /b/a-d/oN *.nupkg') do (
+for /f "delims=" %%a in ('dir /b/a-d/oN %~dp0..\..\..\nuget\*.nupkg') do (
     echo ""
     echo %%a: 开始推送
-    dotnet nuget push "%%a" --api-key "%GB_NUGET_TOKEN%" --source "%source_url%" --skip-duplicate
+    dotnet nuget push "%~dp0..\..\..\nuget\%%a" --api-key "%1" --source "%2" --skip-duplicate
     echo %%a: 推送完成
 )
