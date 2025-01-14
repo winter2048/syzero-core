54106eded6983209c239af77e261bdf3a10f4afc
winter2048 <522112669@qq.com>
2025-01-14 23:26:34 +0800

    update

---
+++ src/SyZero.Core/SyZero.Nacos/ServiceManagement.cs
@@ -0,0 +1,54 @@
+﻿using Nacos.V2;
+using Nacos.V2.Naming.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using SyZero.Cache;
+using SyZero.Runtime.Security;
+using SyZero.Service;
+using ServiceInfo = SyZero.Service.ServiceInfo;
+
+namespace SyZero.Nacos
+{
+    public class ServiceManagement : IServiceManagement
+    {
+        private readonly INacosNamingService _nacosNamingService;
+        private readonly ICache _cache;
+
+        public ServiceManagement(INacosNamingService nacosNamingService, ICache cache)
+        {
+            this._nacosNamingService = nacosNamingService;
+            this._cache = cache;
+        }
+
+        public async Task<List<ServiceInfo>> GetService(string serviceName)
+        {
+            if (_cache.Exist($"Consul:{serviceName}"))
+            {
+                return _cache.Get<List<ServiceInfo>>($"Consul:{serviceName}");
+            }
+            else
+            {
+                // 这里需要知道被调用方的服务名
+                var services = await _nacosNamingService.SelectInstances(serviceName, true);
+                if (services.Count == 0)
+                {
+                    throw new Exception($"SyZero.Consul:未找到{serviceName}服务!");
+                }
+                var serviceInfos = services.Select(service => new ServiceInfo()
+                {
+                    ServiceID = service.InstanceId,
+                    ServiceName = service.ServiceName,
+                    ServiceAddress = service.Ip,
+                    ServicePort = service.Port,
+                    ServiceProtocol = service.Metadata.TryGetValue("secure", out _) ? ProtocolType.HTTPS : ProtocolType.HTTP
+                }).ToList();
+                _cache.Set($"Consul:{serviceName}", serviceInfos, 30);
+                return serviceInfos;
+            }
+        }
+    }
+}
---
+++ src/SyZero.Core/SyZero.Nacos/SyZeroNacosExtension.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nacos.AspNetCore.V2;
+using System;
+using SyZero.Nacos;
+using SyZero.Service;
+
+namespace SyZero
+{
+    /// <summary>
+    /// Nacos配置扩展
+    /// </summary>
+    public static class SyZeroNacosExtension
+    {
+        //
+        // 摘要:
+        //     Add Nacos AspNet. This will register and de-register instance automatically.
+        //     Mainly for nacos server 2.x
+        //
+        // 参数:
+        //   services:
+        //     services.
+        //
+        //   configuration:
+        //     configuration
+        //
+        //   section:
+        //     section, default is nacos
+        //
+        // 返回结果:
+        //     IServiceCollection
+        public static IServiceCollection AddNacos(this IServiceCollection services, IConfiguration configuration, string section = "nacos")
+        {
+            services.AddSingleton<IServiceManagement, ServiceManagement>();
+            return services.AddNacosAspNet(configuration, section);
+        }
+
+        //
+        // 摘要:
+        //     Add Nacos AspNet. This will register and de-register instance automatically.
+        //     Mainly for nacos server 2.x
+        //
+        // 参数:
+        //   services:
+        //     services
+        //
+        //   optionsAccs:
+        //     optionsAccs
+        //
+        // 返回结果:
+        //     IServiceCollection
+        public static IServiceCollection AddNacos(this IServiceCollection services, Action<NacosAspNetOptions> options)
+        {
+            services.AddSingleton<IServiceManagement, ServiceManagement>();
+            return services.AddNacosAspNet(options);
+        }
+    }
+}
