4237bbad6b0a4af11aa93eb7f7d6d53856578f20
HaitaoJin <v-haitaojin@microsoft.com>
2025-06-26 16:38:46 +0800

    update

--- src/SyZero.Core/SyZero.SqlSugar/SyZeroSqlSugarExtension.cs
+++ src/SyZero.Core/SyZero.SqlSugar/SyZeroSqlSugarExtension.cs
@@ -93,7 +93,7 @@ namespace SyZero
             //注册上下文
             services.AddScoped<ISyZeroDbContext, TContext>();
             //注册仓储泛型
-            //services.AddScoped(typeof(IRepository<>), typeof(SqlSugarRepository<>));
+            services.AddScoped(typeof(IRepository<>), typeof(SqlSugarRepository<>));
             services.AddClassesAsImplementedInterface(typeof(IRepository<>));
             ////注册持久化
             services.AddScoped<IUnitOfWork, UnitOfWork>();
